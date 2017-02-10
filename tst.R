f449e64f9a05d62a1bf5c787726fa0b80642086a
maurolepore <maurolepore@gmail.com>
2017-02-10 08:06:05 -0500

    Take  notes from database meeting. Upadate slides to demonstrate dplyr

---
+++ tst.R
@@ -0,0 +1,15 @@
+# https://bookdown.org/maurolepore/visualize_wrangle/visualize_wrangle.html#3
+
+# Packages ----------------------------------------------------------------
+
+# If needed, install
+# install.packages("dplyr")
+# install.packages("nycflights13")
+
+# Load
+library(dplyr)
+library("nycflights13")
+
+# Play --------------------------------------------------------------------
+
+
