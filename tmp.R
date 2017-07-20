7f7d56d0e79208731f9093d0598c11524b00df6d
maurolepore <maurolepore@gmail.com>
2017-07-20 08:26:38 -0500

    Add titles to slides and update some images

--- tmp.R
+++ tmp.R
@@ -1,8 +1,27 @@
-# install.packages("devtools")
-devtools::install_github(forestgeo/bci)
-library(bci)
+forestgeo_forestr_update_puerto_rico <- "Demo"
 
+
+# Documentation ----
+
+# Install first: https://forestgeo.github.io/forestr/
+library(forestr)
+
+?forestr
+?abundance
+
+
+
+# Packaged data ----
+
+# Install first: https://forestgeo.github.io/bci/
+library(bci)
 ?bci
+
 str(bci12full1)
+
+# DONT RUN
+# Notice autocompletion
 bci
+
+
 
